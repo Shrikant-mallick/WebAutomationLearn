d3a9d4ac73da44b25ba62e52940f4f299fe2b15f
Shrikant-mallick <mailtoshrikantm@gmail.com>
2019-11-17 21:55:15 +0530

    RandomString Generator

---
+++ src/main/java/WTW/RandomStringGen.java
@@ -0,0 +1,27 @@
+package WTW;
+
+import java.util.Random;
+
+public class RandomStringGen {
+
+	
+	public static String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 8) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(getSaltString());
+	}
+
+}
