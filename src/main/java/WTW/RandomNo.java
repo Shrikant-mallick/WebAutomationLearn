c7a14909ecb71083a49b18272b1669936dd72436
TendulkarHR <htendulkar10@gmail.com>
2019-11-23 21:42:03 +0530

    New Files

--- src/main/java/WTW/RandomNo.java
+++ src/main/java/WTW/RandomNo.java
@@ -16,17 +16,6 @@ public class RandomNo {
 //          //System.out.println(Math.random());
         }
 		
-//		String SALTCHARS = "1234567890";
-//        StringBuilder salt = new StringBuilder();
-//        Random rnd = new Random();
-//        while (salt.length() < 10) 
-//        { 
-//        	// length of the random string.
-//            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-//            salt.append(SALTCHARS.charAt(index));
-//        }
-//        String saltStr = salt.toString();
-//        return saltStr;
 	}
 
 }
