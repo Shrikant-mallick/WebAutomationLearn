3b7f2fc2eed1f396f5545d61fe6fcedea227729c
TendulkarHR <htendulkar10@gmail.com>
2019-11-23 22:12:20 +0530

    New code added

---
+++ src/main/java/stringOperations/MyStrings.java
@@ -0,0 +1,12 @@
+package stringOperations;
+
+public class MyStrings
+{
+	public void indexsearch()
+	{
+		String Char = "This is a learning Program for CharAt String Operations.";
+	
+		System.out.println("CharAt : "+ Char.charAt(25));
+	}
+}
+	
--- src/main/java/stringOperations/OperationsString.java
+++ src/main/java/stringOperations/OperationsString.java
@@ -1,9 +1,13 @@
 package stringOperations;
 
-public class OperationsString {
+public class OperationsString 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		MyStrings st = new MyStrings();
+		
+		st.indexsearch();
 
 	}
 
